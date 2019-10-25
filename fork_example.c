a762fecd682ea5c54fe0c37f2d309750cf35d002
sinan <seenone@gmail.com>
2019-10-25 11:59:40 +0300

    format the code

--- fork_example.c
+++ fork_example.c
@@ -9,16 +9,19 @@
 int main()
 {
     pid_t pid;
-	pid = fork();
-	
-	if (pid < 0) {  
-		fprintf(stderr, "Fork Oluşturulamadı");
-	}
-	else if (pid == 0) { /* çocuk process */
-		execlp("/bin/ls", "ls", NULL);
+    pid = fork();
+
+    if (pid < 0)
+    {
+        fprintf(stderr, "Fork Oluşturulamadı");
+    }
+    else if (pid == 0)
+    { /* çocuk process */
+        execlp("/bin/ls", "ls", NULL);
         printf("burası asla yazmaz");
-	}
-    else {
+    }
+    else
+    {
         wait(NULL);
         printf("Çocuk process bitti\n");
     }
