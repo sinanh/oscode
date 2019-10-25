9abda8dc0964e53ff6488dd05080c2b8c3cf614a
sinan <seenone@gmail.com>
2019-10-25 11:55:27 +0300

    fork örneği

---
+++ fork_example.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+// Silberschatz kitabındaki fork örneği
+
+int main()
+{
+    pid_t pid;
+	pid = fork();
+	
+	if (pid < 0) {  
+		fprintf(stderr, "Fork Oluşturulamadı");
+	}
+	else if (pid == 0) { /* çocuk process */
+		execlp("/bin/ls", "ls", NULL);
+        printf("burası asla yazmaz");
+	}
+    else {
+        wait(NULL);
+        printf("Çocuk process bitti\n");
+    }
+}
